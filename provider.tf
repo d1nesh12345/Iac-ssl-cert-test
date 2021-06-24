986f14f4aaf267f08ae9bbe488a351390019b881
d1nesh12345 <36250257+d1nesh12345@users.noreply.github.com>
2021-06-23 20:10:46 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
-provider "google-beta" {
+provider "google" {
   project     = "qwiklabs-gcp-00-5aceec377a09"
+  source  = "hashicorp/google"
   region      = "us-central1"
   zone        = "us-central1-c"
 }
