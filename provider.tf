7d1354fe3a298481d55df708ffc64bb21df580ff
d1nesh12345 <36250257+d1nesh12345@users.noreply.github.com>
2021-06-23 23:12:23 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project     = "qwiklabs-gcp-04-eeb0254dc548"
+  project     = "qwiklabs-gcp-04-75739a41967a"
   credentials = "${file("/tmp/key.json")}"
   region      = "us-central1"
   zone        = "us-central1-c"
