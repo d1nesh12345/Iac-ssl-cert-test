dd1e104a42b2100c466c6936a3ff264d7d595a45
d1nesh12345 <36250257+d1nesh12345@users.noreply.github.com>
2021-06-24 19:48:51 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  default = "demo-cluster"
+}
+
+variable "location" {
+  default = "us-central1"
+}
+
+variable "initial_node_count" {
+  default = 2
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
