0f0f39745fe54b659d7850a9ef03e22ce2f1defe
d1nesh12345 <36250257+d1nesh12345@users.noreply.github.com>
2021-06-24 12:13:48 -0400

    Create FW.tf

---
+++ FW.tf
@@ -0,0 +1,11 @@
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http-rule"
+  network = "default"
+  allow {
+    ports    = ["80"]
+    protocol = "tcp"
+  }
+  target_tags = ["allow-http"]
+  priority    = 1000
+
+}
