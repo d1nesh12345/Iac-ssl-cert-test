0e830c7eb8f8d7894366de585b8ea849bff93e99
d1nesh12345 <36250257+d1nesh12345@users.noreply.github.com>
2021-06-25 12:43:08 -0400

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -38,3 +38,40 @@ resource "google_container_node_pool" "default" {
     ]
   }
 }
+  
+  resource "kubernetes_service" "example" {
+  metadata {
+    name = "ingress-service"
+  }
+  spec {
+    port {
+      port = 80
+      target_port = 80
+      protocol = "TCP"
+    }
+    type = "NodePort"
+  }
+}
+
+resource "kubernetes_ingress" "example" {
+  wait_for_load_balancer = true
+  metadata {
+    name = "example"
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
+  spec {
+    rule {
+      http {
+        path {
+          path = "/*"
+          backend {
+            service_name = kubernetes_service.example.metadata.0.name
+            service_port = 80
+          }
+        }
+      }
+    }
+  }
+}
