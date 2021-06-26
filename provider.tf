9e11c215a7c1b9152bfc4dc3410b190b00be5453
d1nesh12345 <36250257+d1nesh12345@users.noreply.github.com>
2021-06-26 17:35:52 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,4 +3,5 @@ provider "google" {
   credentials = "${file("/tmp/key.json")}"
   region      = "us-central1"
   zone        = "us-central1-c"
+  service = [ "iam.googleapis.com", "container.googleapis.com", "monitoring.googleapis.com", "cloudtrace.googleapis.com", "clouddebugger.googleapis.com", "cloudprofiler.googleapis.com"]
 }
