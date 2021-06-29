baee16d4fddb2eb9b3d07016082deff0e0c3b626
d1nesh12345 <36250257+d1nesh12345@users.noreply.github.com>
2021-06-28 21:45:44 -0400

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -49,6 +49,11 @@ resource "google_compute_instance" "default1" {
 
  metadata_startup_script = "yum install httpd -y;systemctl start httpd; systemctl enable httpd.service;firewall-cmd --add-service=http --permanent;firewall-cmd --reload;echo WEBSERVER_1 > /var/www/html/webserver;echo I_AM_HEALTHY > /var/www/html/healthcheck;yum install git -y;mkdir /var/www/html/site;git clone https://github.com/d1nesh12345/Iac-site --branch=master /var/www/html/site"
   //metadata_startup_script = "echo hi > /test.txt"
+  
+  wait_for_completion = true
+ timeouts {
+   create = "40s"
+ }
 }
 
 resource "google_compute_instance" "default2" {
@@ -84,4 +89,9 @@ resource "google_compute_instance" "default2" {
     metadata_startup_script = "yum install httpd -y;systemctl start httpd; systemctl enable httpd.service;firewall-cmd --add-service=http --permanent;firewall-cmd --reload;echo WEBSERVER_1 > /var/www/html/webserver;echo I_AM_HEALTHY > /var/www/html/healthcheck;yum install git -y;mkdir /var/www/html/site;git clone https://github.com/d1nesh12345/Iac-site --branch=master /var/www/html/site"
  //metadata_startup_script = "echo hi > /test.txt"
 
+  
+  wait_for_completion = true
+ timeouts {
+   create = "40s"
+ }
 }
