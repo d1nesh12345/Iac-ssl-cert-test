89ff0285f1e5240279204b83ce31f84e1a225a97
d1nesh12345 <36250257+d1nesh12345@users.noreply.github.com>
2021-06-30 10:40:42 -0400

    Update External-lb.tf

--- External-lb.tf
+++ External-lb.tf
@@ -1,21 +1,49 @@
-resource "google_compute_forwarding_rule" "default" {
-  provider              = google
-  name                  = "website-forwarding-rule"
-  region                = "us-central1"
-  port_range            = 80
-  backend_service       = google_compute_region_backend_service.backend.id
-}
-resource "google_compute_region_backend_service" "backend" {
-  provider              = google
-  name                  = "website-backend"
-  region                = "us-central1"
-  load_balancing_scheme = "EXTERNAL"
-  health_checks         = [google_compute_region_health_check.hc.id]
-  backend {
-    group = google_compute_instance_group.test_grp.id
+resource "google_compute_managed_ssl_certificate" "default" {
+  name = "test-cert"
+
+  managed {
+    domains = ["sslcert.tf-test.club."]
   }
 }
-resource "google_compute_region_health_check" "hc" {
+
+resource "google_compute_target_https_proxy" "default" {
+  name             = "test-proxy"
+  url_map          = google_compute_url_map.default.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.default.self_link]
+}
+
+resource "google_compute_url_map" "default" {
+  name        = "url-map"
+  description = "a description"
+
+  default_service = google_compute_backend_service.default.self_link
+
+  host_rule {
+    hosts        = ["sslcert.tf-test.club"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_backend_service.default.self_link
+
+    path_rule {
+      paths   = ["/*"]
+      service = google_compute_backend_service.default.self_link
+    }
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  name        = "backend-service"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_http_health_check.default.self_link]
+}
+
+resource "google_compute_http_health_check" "default" {
   provider           = google
   name        = "http-health-check"
   description = "Health check via http"
@@ -34,3 +62,22 @@ resource "google_compute_region_health_check" "hc" {
   }
 }
 
+resource "google_dns_managed_zone" "zone" {
+  name     = "dnszone"
+  dns_name = "sslcert.tf-test.club."
+}
+
+resource "google_compute_global_forwarding_rule" "default" {
+  name       = "forwarding-rule"
+  target     = google_compute_target_https_proxy.default.self_link
+  region     = "us-central1"
+  port_range = 443
+}
+
+resource "google_dns_record_set" "set" {
+  name         = "sslcert.tf-test.club."
+  type         = "A"
+  ttl          = 3600
+  managed_zone = google_dns_managed_zone.zone.name
+  rrdatas      = [google_compute_global_forwarding_rule.default.ip_address]
+}
